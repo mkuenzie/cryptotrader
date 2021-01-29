175db21211d3a43221e1768e90bdf465928451e9
mkuenzie <mkuenzie>
2021-01-29 08:16:04 -0800

    better state memory w/ csv trade history

--- cryptotrader/cryptotrader.py
+++ cryptotrader/cryptotrader.py
@@ -1,5 +1,6 @@
-
 import os
+from csv import DictWriter, DictReader
+
 import pandas as pd
 from bittrex.bittrex import Bittrex, CandleInterval
 from datetime import datetime, timedelta
@@ -8,7 +9,8 @@ from datetime import datetime, timedelta
 class Cryptotrader(object):
 
     def __init__(self, market, strategy, interval=timedelta(minutes=1), action='BUY', fee=0):
-        self.exchange = Bittrex(api_key=os.environ.get("BITTREX_API_KEY"), api_secret=os.environ.get("BITTREX_API_SECRET"))
+        self.exchange = Bittrex(api_key=os.environ.get("BITTREX_API_KEY"),
+                                api_secret=os.environ.get("BITTREX_API_SECRET"))
         self.market = market
         self.market_data = pd.DataFrame()
         self.market_start = datetime.now()
@@ -69,7 +71,7 @@ class Cryptotrader(object):
             if curr_action == 'BUY':
                 buy_price = self.strategy.enter(sliced_data, tick_data.close.item())
                 if tick_low <= buy_price <= tick_high:
-                    crypto_wallet = round(usd_wallet/buy_price, 6)
+                    crypto_wallet = round(usd_wallet / buy_price, 6)
                     commission = round(usd_wallet * self.fee, 2)
                     usd_wallet = 0
                     trades.append({'timestamp': curr_tick, 'action': curr_action,
@@ -80,7 +82,7 @@ class Cryptotrader(object):
                 if tick_low <= sell_price <= tick_high:
                     commission = round(crypto_wallet * sell_price * self.fee, 2)
                     trades.append({'timestamp': curr_tick, 'action': curr_action,
-                                   'price': sell_price,  'amount': crypto_wallet, 'fee': commission})
+                                   'price': sell_price, 'amount': crypto_wallet, 'fee': commission})
                     usd_wallet = round(crypto_wallet * sell_price, 2)
                     crypto_wallet = 0
                     curr_action = 'BUY'
@@ -99,27 +101,76 @@ class Cryptotrader(object):
             return self.exchange.sell(self.market, quantity)
         raise ValueError('Invalid value for action')
 
+    def write_csv(self, trades, filename, append=True):
+        if len(trades) == 0:
+            return
+        csv_columns = list(trades[0].keys())
+        csv_file = filename
+        if append:
+            mode = 'a+'
+        else:
+            mode = 'w'
+        try:
+            with open(csv_file, mode, newline='') as csvfile:
+                dict_writer = DictWriter(csvfile, fieldnames=csv_columns)
+                if not append:
+                    dict_writer.writeheader()
+                for trade in trades:
+                    dict_writer.writerow(trade)
+            csvfile.close()
+        except IOError:
+            print("I/O error")
 
+    def read_csv(self, filename):
+        trades = []
+        with open(filename, 'r') as data:
+            for line in DictReader(data):
+                timestamp = datetime.strptime(line['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+                trade = {
+                    'timestamp': timestamp,
+                    'action': line['action'],
+                    'price': float(line['price']),
+                    'amount': float(line['amount']),
+                    'fee': float(line['fee'])
+                }
+                trades.append(trade)
+        return trades
 
-# init
-# setup exchange, strategy, market data
-#
-# refresh
-#update market & ticker data
-#
-# eval
-# find buy/sell strike prices from set strategy
-#
-# test
-#
-# walk current market_data set and output trades strategy would make
-#
-# strike
-# buy/sell at market
-#
-# trade
-# action strategy on a market
-#
-#
+    def generate_markdown(self, trades, initial_wallet, usd_wallet, crypto_wallet):
+        total = len(trades)
+        markdown_text = "# Cryptotrader Stats\n\n"
+        markdown_text += "Initial Wallet: $" + str(initial_wallet) + "\n"
+        if usd_wallet == 0:
+            wallet = round(crypto_wallet * self.exchange.get_ticker(), 2)
+        else:
+            wallet = round(usd_wallet, 2)
+        markdown_text += "Current Wallet: $" + str(wallet) + "\n\n"
+        markdown_text += "Gain/Loss: "
+        if wallet >= initial_wallet:
+            markdown_text += '+ %.2f\n' % (((wallet / initial_wallet) - 1) * 100)
+        else:
+            markdown_text += '- %.2f\n' % ((1 - (wallet / initial_wallet)) * 100)
+        markdown_text += "### Trades \n"
+        markdown_text += "| Buy Date | Buy @ Price | Sell Date | Sell @ Price | Start/End Amount | Gain/Loss |\n"
+        markdown_text += "| :------------- | :----------: | -----------: | :------------- | :----------: | -----------: |"
+        for i in range(0, total - 1, 2):
+            buy = trades[i]
+            sell = trades[i + 1]
+            buy_price = buy['price']
+            buy_amnt = buy['amount']
+            sell_price = sell['price']
+            sell_amnt = sell['amount']
+            buy_value = round(buy_price * buy_amnt, 2)
+            sell_value = round(sell_price * sell_amnt, 2)
+            markdown_text += "| " + str(buy['timestamp']) + " | " + str(buy_amnt) + '@' + str(buy_price) + " | " \
+                             + str(sell['timestamp']) + " | " + str(sell_amnt) + ' @ ' + str(sell_price) + " | " \
+                             + str(buy_value) + '/' + str(sell_value) + " | "
+            if buy_price <= sell_price:
+                gain = ((sell_price / buy_price) - 1) * 100
+                markdown_text += ("+ %.2f" % gain) + " |\n"
+            else:
+                loss = (1 - (sell_price / buy_price)) * 100
+                markdown_text += ('- %.2f' % loss) + " |\n"
+        return markdown_text
 
-
+#
--- main.py
+++ main.py
@@ -6,74 +6,24 @@ from csv import DictWriter
 import time
 
 initial_wallet = 50
-usd_wallet = 50
+usd_wallet = 54.83
 crypto_wallet = 0
 trades = []
 action = 'BUY'
 
 trader = Cryptotrader(market='BTC-USD', strategy=BbandsStrategy3(proximity=0.25, stddevs=2), fee=0.0035, interval=timedelta(hours=1))
+trades = trader.read_csv('trades.csv')
 
-
-def generate_markdown():
-    total = len(trades)
-    markdown_text = "# Cryptotrader Stats\n\n"
-    markdown_text += "Initial Wallet: $" + str(initial_wallet) + "\n"
-    if usd_wallet == 0:
-        wallet = round(crypto_wallet * trader.get_ticker(), 2)
-    else:
-        wallet = round(usd_wallet, 2)
-    markdown_text += "Current Wallet: $" + str(wallet) + "\n\n"
-    markdown_text += "Gain/Loss: "
-    if wallet >= initial_wallet:
-        markdown_text += '+ %.2f\n' % (((wallet / initial_wallet) - 1) * 100)
-    else:
-        markdown_text += '- %.2f\n' % ((1 - (wallet / initial_wallet)) * 100)
-    markdown_text += "### Trades \n"
-    markdown_text += "| Buy Date | Buy @ Price | Sell Date | Sell @ Price | Start/End Amount | Gain/Loss |\n"
-    markdown_text += "| :------------- | :----------: | -----------: | :------------- | :----------: | -----------: |"
-    for i in range(0, total - 2, 2):
-        buy = trades[i]
-        sell = trades[i + 1]
-        buy_price = buy['price']
-        buy_amnt = buy['amount']
-        sell_price = sell['price']
-        sell_amnt = sell['amount']
-        buy_value = round(buy_price * buy_amnt, 2)
-        sell_value = round(sell_price * sell_amnt, 2)
-        markdown_text += "| " + str(buy['timestamp']) + " | " + str(buy_amnt) + '@' + str(buy_price) + " | " \
-                         + str(sell['timestamp']) + " | " + str(sell_amnt) + ' @ ' + str(sell_price) + " | " \
-                         + str(buy_value) + '/' + str(sell_value) + " | "
-        if buy_price <= sell_price:
-            gain = ((sell_price / buy_price) - 1) * 100
-            markdown_text += ("+ %.2f" % gain) + " |\n"
-            print('+ %.2f' % gain)
-        else:
-            loss = (1 - (sell_price / buy_price)) * 100
-            markdown_text += ('- %.2f' % loss) + " |\n"
-    return markdown_text
-
-
-def write_csv():
-    csv_columns = list(trades[0].keys())
-    csv_file = 'trades.csv'
-    try:
-        with open(csv_file, 'a+', newline='') as csvfile:
-            dict_writer = DictWriter(csvfile, fieldnames=csv_columns)
-            for trade in trades:
-                dict_writer.writerow(trade)
-        csvfile.close()
-    except IOError:
-        print("I/O error")
-
-
-def write_markdown():
-    mdfile = generate_markdown()
+def write_markdown(trades, initial_wallet, usd_wallet, crypto_wallet):
+    mdfile = trader.generate_markdown(trades, initial_wallet, usd_wallet, crypto_wallet)
     f = open('readme.md', 'w')
     f.write(mdfile)
     f.close()
 
 
+
 def trade():
+    session_start = datetime.utcnow()
     while True:
         global usd_wallet, crypto_wallet, action
         refresh_success = trader.refresh()
@@ -127,10 +77,10 @@ def trade():
                 usd_wallet = fill_quantity
                 crypto_wallet = 0
                 action = 'BUY'
-            write_markdown()
+            write_markdown(trades, initial_wallet, usd_wallet, crypto_wallet)
+            new_trades = [d for d in trades if d['timestamp'] > session_start]
             if len(trades) > 0:
-                write_csv()
-
+                trader.write_csv(new_trades, 'trades.csv', append=True)
         print("Wait 1 min...")
         time.sleep(60)
 
